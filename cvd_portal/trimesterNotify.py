ac6d6f7a1d4f3f2f072600b6a812d692beea34a1
sahilom94 <sahilom94@gmail.com>
2020-04-13 14:06:45 +0530

    Stable Commit

--- cvd_portal/trimesterNotify.py
+++ cvd_portal/trimesterNotify.py
@@ -11,34 +11,36 @@ def trimesterNotifyFunc(data) :
 
     for patient in Patient.objects.all() :
         try :
-            lmpDate = (date.today() - patient.lmp.date()).days
-
-            notifMessage = ''
-            send_message = 0
-
-            if(lmpDate <= 90) :
-                notifMessage = data['trimesterOneText']
-                send_message = 1
-            elif (lmpDate > 90 and lmpDate <= 180) :
-                notifMessage = data['trimesterTwoText']
-                send_message = 1
-            elif(lmpDate > 180 and lmpDate <= 300) :
-                notifMessage = data['trimesterThreeText']
-                send_message = 1
 
             # doctor = Doctor.objects.get(patient.doctor)
             if(patient.doctor and data['doctorId'] == patient.doctor.pk):
-                print("Patient Name: ", patient, "Doctor Id: ", patient.doctor.pk,  "LMP Date: ", patient.lmp, "Days Past from LMP: ", lmpDate, "Message: ", notifMessage)
 
-            # if(send_message == 1) :
-            #     _to = patient.patient_id.device.device_id
-            #     message = notifMessage
-            #     body = {'to': _to, 'data': {"message": message}}
-            #     body = json.dumps(body).encode('utf8')
-            #     headers = {
-            #         'content-type': 'application/json',
-            #         'Authorization': 'key=' + 'AAAAoaPpcJo:APA91bHMg0Cgj7nkunnr7vxphja5Zetxkk6FjDQ1oB-sHf4YNU8IUyo1o_XDcUOTohKsyf2T061i4Wix4DkDmHJfgYZLebhcgcVhPk25zft8sjOyZlTjGJRuCyuhzpJrkdDtZvseE_P9'
-            #         }
-            #     r = requests.post(url, data=body, headers=headers)
+                lmpDate = (date.today() - patient.lmp.date()).days
+
+                notifMessage = ''
+                send_message = 0
+
+                if(lmpDate <= 90) :
+                    notifMessage = data['trimesterOneText']
+                    send_message = 1
+                elif (lmpDate > 90 and lmpDate <= 180) :
+                    notifMessage = data['trimesterTwoText']
+                    send_message = 1
+                elif(lmpDate > 180 and lmpDate <= 300) :
+                    notifMessage = data['trimesterThreeText']
+                    send_message = 1
+
+                print("Patient Name: ", patient, "Doctor Id: ", patient.doctor.pk, "Phone Id: ", patient.device.device_id ,"LMP Date: ", patient.lmp, "Days Past from LMP: ", lmpDate, "Message: ", notifMessage)
+
+                if(send_message == 1) :
+                    _to = patient.device.device_id
+                    message = notifMessage
+                    body = {'to': _to, 'data': {"message": message}}
+                    body = json.dumps(body).encode('utf8')
+                    headers = {
+                        'content-type': 'application/json',
+                        'Authorization': 'key=' + 'AAAAoaPpcJo:APA91bHMg0Cgj7nkunnr7vxphja5Zetxkk6FjDQ1oB-sHf4YNU8IUyo1o_XDcUOTohKsyf2T061i4Wix4DkDmHJfgYZLebhcgcVhPk25zft8sjOyZlTjGJRuCyuhzpJrkdDtZvseE_P9'
+                        }
+                    r = requests.post(url, data=body, headers=headers)
 
         except Exception as e: print(e)
